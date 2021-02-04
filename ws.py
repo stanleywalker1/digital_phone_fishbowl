cd56c527140ded43bc81b70127cc4dd817d9a695
Wendy Ju <1128669+wendyju@users.noreply.github.com>
2021-02-04 01:15:16 -0500

    Delete ws.py

--- ws.py
+++
@@ -1,9 +0,0 @@
-from websocket import create_connection
-ws = create_connection("ws://192.168.1.165:3000/")
-print("Sending 'Hello, World'...")
-ws.send("Hello, World")
-print("Sent")
-print("Receiving...")
-result =  ws.recv()
-print("Received '%s'" % result)
-ws.close()
